06748fd5326c6c1e0ee6452fe7e6db1180d2a9d2
MATHEUS HENRIQUE SALDIAS PALACIOS PIETRO BIAS <86833526+Ma7heus@users.noreply.github.com>
2022-09-01 21:14:25 -0300

    Adicionando exercicios de condicionais

---
+++ src/logic_java_exercises/condicionaveis.java
@@ -0,0 +1,15 @@
+package logic_java_exercises;
+
+public class condicionaveis {
+	public static void main(String[] args) {
+		
+		int idade = 21;
+		
+		if (idade>=18) {
+			System.out.println("Voce e maior de idade, ja pode lokiar!");
+		} else {
+			System.out.println("Voce e menor de idade, volta ver galinha pintadinha!");
+		}	
+		
+	}
+}
---
+++ src/logic_java_exercises/variasCondicionais.java
@@ -0,0 +1,38 @@
+package logic_java_exercises;
+
+public class variasCondicionais {
+	public static void main(String[] args) {
+		
+		int mes = 5;
+		switch (mes) {
+		
+		case 1:
+			System.out.println("Janeiro");
+			break;
+		case 2:
+			System.out.println("Fevereiro");
+			break;
+		case 3:
+			System.out.println("Março");
+			break;
+		case 4:
+			System.out.println("Abril");
+			break;
+		case 5:
+			System.out.println("Maio");
+			break;
+		case 6:
+			System.out.println("Junho");
+			break;
+		case 7:
+			System.out.println("Julho");
+			break;
+		case 8:
+			System.out.println("Agosto");
+			break;
+			
+		}
+		
+		
+	}
+}
